087a7726306871ffef7bb525a4a4cd4c86572de8
Ashmit T. <207732305+ashmitjr@users.noreply.github.com>
2026-03-09 15:18:08 +0000

    fix: clean main.jsx - remove duplicate imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,19 +13,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </PersistGate>
   </Provider>
 );
-EOcat > src/main.jsx << 'EOF'
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./app/store";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
-);
